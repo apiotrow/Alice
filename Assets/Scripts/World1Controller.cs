37b509cd78e491ea9f0e138506812e38325c5dc9
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-09-05 00:58:59 -0700

    Executable

---
+++ Assets/Scripts/World1Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World1Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 start = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+		Vector3 end = new Vector3 (transform.eulerAngles.x, start.y - 0.1f, transform.eulerAngles.z);
+		transform.eulerAngles = end;
+	}
+}
