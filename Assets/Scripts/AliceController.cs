0d26463f7a1fed006d7546222dc701ff9880a7e7
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-10-04 03:47:18 -0700

    mobile controller improved, new dragging feature

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -82,7 +82,7 @@ public class AliceController : MonoBehaviour
 		RenderSettings.fog = false;
 		RenderSettings.fogEndDistance = minScale * fogEndDistanceMultiplier;
 
-		manualSizeChanging = true;
+		manualSizeChanging = false;
 	}
 
 
